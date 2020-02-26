0d7ca145da1374dad07b861e9fc4613a6cd7e37a
656Krona <ykaskrmr@gmail.com>
2020-02-26 04:55:08 +0000

    修正_コメント投稿非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,7 @@ class BookCommentsController < ApplicationController
     #binding.pry
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = book.id
+    @book_comment.book_id = @book.id
     if @book_comment.save
     #redirect_to book_path(book), notice: "successfully created comment!"
     else
